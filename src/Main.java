8905f8086085680414c29f1e35e56f5b42317e98
Obmor <terekhin.alex@mail.ru>
2024-08-27 18:05:17 +0300

    HW4.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ageOfClient = 22;
+        int salary = 80000;
+        if (ageOfClient < 23 && salary >= 50000 && salary < 80000) {
+            System.out.println("До 23, 50-80 лимит " + salary * 2 / 100 * 120);
+        } else if (ageOfClient < 23 && salary >= 80000) {
+            System.out.println("До 23, от 80 лимита " + salary * 2 / 100 * 150);
+        } else if (ageOfClient >= 23 && salary >= 50000 && salary < 80000) {
+            System.out.println("После 23, 50-80 лимита " + salary * 3 / 100 * 120);
+        } else if (ageOfClient >= 23 && salary >= 80000) {
+            System.out.println("После 23, от 80 лимита " + salary * 3 / 100 * 150);
+        }
+
+
     }
 }
